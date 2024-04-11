54bccbde66f90eda5be645d01b0175c02f7ebeba
AccountForMe <157073214+AccountForMe@users.noreply.github.com>
2024-04-11 16:49:47 -0400

    idk

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var combination = [0, 0, 0]; // Initial combination
+var maxDigitValue = 9; // Maximum digit value
+
 function toggleMenu() {
     var menu = document.querySelector('.menu-container');
     if (menu.style.right === '-300px') {
@@ -7,3 +10,19 @@ function toggleMenu() {
     }
 }
 
+
+
+function changeDigit(change, digitIndex) {
+    combination[digitIndex] += change;
+    if (combination[digitIndex] < 0) {
+        combination[digitIndex] = maxDigitValue;
+    } else if (combination[digitIndex] > maxDigitValue) {
+        combination[digitIndex] = 0;
+    }
+    document.getElementById('digit' + (digitIndex + 1)).innerText = combination[digitIndex];
+}
+
+function checkCombination() {
+    // Implement your logic here to check the combination
+    alert('Combination: ' + combination.join(''));
+}
